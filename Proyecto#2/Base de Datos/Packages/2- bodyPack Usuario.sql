449bb355510901c8fe8a8cdbfb594df8d994b946
fransa2910 <franco.solis2910@hotmail.com>
2014-05-14 01:24:32 -0600

    Update and rename 2- bodyPack Perfil.sql to 2- bodyPack Usuario.sql

---
+++ Proyecto#2/Base de Datos/Packages/2- bodyPack Usuario.sql
@@ -0,0 +1,90 @@
+CREATE OR REPLACE PACKAGE BODY pack_usuario AS
+
+     --Funcion get nombre
+     FUNCTION get_usuario(cedulaUsuario NUMBER)
+          RETURN VARCHAR2
+     IS
+          nickName VARCHAR2(25);
+     BEGIN
+          SELECT usuario
+          INTO nickName
+          FROM usuario
+          WHERE usuario.cedulaUsuario_id = cedulaUsuario;
+
+          EXCEPTION
+
+          WHEN NO_DATA_FOUND THEN
+               DBMS_OUTPUT.put_line('La cedula es inválido');
+          RETURN(nickName);
+     END;
+
+     -- funcion get cedula
+     FUNCTION get_cedula(usuario VARCHAR2)
+          RETURN NUMBER
+          IS
+               usuarioId NUMBER(9);
+
+          BEGIN
+               SELECT cedulaUsuario_id
+               INTO usuarioId
+               FROM usuario
+               WHERE usuario.usuario = usuario;
+
+               EXCEPTION
+                    WHEN NO_DATA_FOUND THEN
+                         DBMS_OUTPUT.put_line('El nickName es inválido');
+
+               RETURN(usuarioId);
+
+     END;
+
+     --Procedimiento para insertar personas
+     PROCEDURE set_usuario(cedulaUsuario NUMBER, usuario VARCHAR2, contrasena VARCHAR2, privacidad NUMBER, estado NUMBER)
+          IS
+          BEGIN
+               INSERT INTO usuario
+                    (cedulaUsuario_id, usuario, contrasena, privacidad, estado)
+               VALUES
+                    (cedulaUsuario, usuario, contrasena, privacidad, estado);
+  	       COMMIT;
+     END;
+
+     --Procedimiento para eliminar personas
+     PROCEDURE del_usuario(cedulaUsuario NUMBER)
+          IS
+          BEGIN
+               DELETE FROM usuario
+               WHERE usuario.cedulausuario_id = cedulaUsuario;
+	       COMMIT;
+     END;
+
+     --Procedimiento para modificar personas
+     PROCEDURE mod_usuario(cedulaUsuario NUMBER, usuario VARCHAR2, contrasena VARCHAR2, privacidad NUMBER)
+          IS
+          BEGIN
+               UPDATE usuario
+               SET (cedulaUsuario_id, usuario, contrasena, privacidad) = (SELECT cedulaUsuario, usuario, contrasena, privacidad FROM DUAL)
+               WHERE usuario.cedulausuario_id = cedulaUsuario;
+	       COMMIT;
+     END;
+     
+      FUNCTION confirmarPassword (Pcontrasena VARCHAR2, pusuario VARCHAR2)
+        RETURN NUMBER
+        IS contra VARCHAR2(30);
+        BEGIN
+          SELECT contrasena
+          INTO contra
+          from usuario
+          where usuario.Usuario = pusuario;
+
+          IF contra = Pcontrasena THEN
+             update usuario
+             set estado = 2
+             where usuario = pusuario;
+             return 1;
+          else
+            RETURN 0;
+          end if;
+      end;
+
+END pack_usuario;
