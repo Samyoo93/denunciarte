080470777e24ff0e50633312ee354798cc00c235
fransa2910 <franco.solis2910@hotmail.com>
2014-05-15 01:00:46 -0600

    Update 14- defPack Direccion_Entidad.sql

--- Proyecto#2/Base de Datos/Packages/14- defPack Direccion_Entidad.sql
+++ Proyecto#2/Base de Datos/Packages/14- defPack Direccion_Entidad.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE PACKAGE pack_direccion_entidad IS
 
      --Procedimiento para llenar la tabla de categoria_personaFisica
-     PROCEDURE set_direccion_entidad(direccion VARCHAR2, barrioId NUMBER, entidadId NUMBER);
+    PROCEDURE set_direccion_entidad (exacta varchar2, nombre_barrio varchar2);
 
 END pack_direccion_entidad;
