b45f2d461f0e6c2d660369e0965fabb4fec38e3a
BarCB2011 <bar_cas_bar@yahoo.com>
2014-05-15 22:30:45 -0600

    Update 18- defPack Reporte_Usuario.sql

--- Proyecto#2/Base de Datos/Packages/18- defPack Reporte_Usuario.sql
+++ Proyecto#2/Base de Datos/Packages/18- defPack Reporte_Usuario.sql
@@ -1,1 +1,6 @@
-18- defPack Reporte_Usuario.sql
+CREATE OR REPLACE PACKAGE pack_reporte_usuario IS
+
+     --Procedimiento para llenar la tabla de reporte_usuario 
+     PROCEDURE set_reporte_usuario(cedulaUsuario NUMBER, reporte_id NUMBER);
+
+END pack_reporte_usuario;
