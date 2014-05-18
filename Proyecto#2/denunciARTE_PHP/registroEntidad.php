2ecc4fb65ff73a9dde1fd52fdf682d0ff481324c
fransa2910 <franco.solis2910@hotmail.com>
2014-05-18 15:23:07 -0600

    Update registroEntidad.php

--- Proyecto#2/denunciARTE_PHP/registroEntidad.php
+++ Proyecto#2/denunciARTE_PHP/registroEntidad.php
@@ -15,7 +15,7 @@
     $distrito = 'Santa Ana centro';//$_POST['distrito'];
     $barrio = 'Machete';//$_POST['barrio'];
     $direccionExacta = 'Del palo de mango 100 oeste al otro lado del chino';//$_POST['direccionExacta'];
-    $cedJuridica = 12412;//$_POST['cedJuridica'];
+    $cedJuridica = 1522237;//$_POST['cedJuridica'];
     /*
     $cedJuridica1 = $_POST['cedJuridica1'];
     $cedJuridica2 = $_POST['cedJuridica2'];
@@ -23,59 +23,120 @@
     $cedJuridica = $cedJuridica1 . $cedJuridica2 . $cedJuridica3;
     $cedJuridica = intval($cedJuridica);
     */
-    $def = "ECHO DE TODO EL HTML IGNORAR POR AHORA";
+    $categoria = 'otra';//$_POST['categoria'];
+    $existe_cat = 1;
+
+   
 
 
     if ($barrio != null and $direccionExacta != null and $cedJuridica != null) {
         //revisa que los campos se llenen
         if(strlen($nombre) < 26 and strlen($cedJuridica) < 10 and strlen($direccionExacta) < 51) {
             //verifica que los cambos tengan un largo correcto
-            
-            //Revisa si la cedula juridica existe ********************************************************
-            $check_ced =  "SELECT COUNT(*) AS NUM_ROWS FROM entidad WHERE cedulajuridica=:cedJuridica";
-            $query_check_ced = ociparse($conn, $check_ced);
-            ocibindbyname($query_check_ced, ":cedJuridica", $cedJuridica);
-            $rows = 0;
-            oci_define_by_name($query_check_ced, "NUM_ROWS", $rows);
-            ociexecute($query_check_ced);
-            ocifetch($query_check_ced);
+            if(is_numeric($cedJuridica)) {
+                    
+                //Revisa si la cedula juridica existe ********************************************************
+                $check_ced =  "SELECT COUNT(*) AS NUM_ROWS FROM entidad WHERE cedulajuridica=:cedJuridica";
+                $query_check_ced = ociparse($conn, $check_ced);
+                ocibindbyname($query_check_ced, ":cedJuridica", $cedJuridica);
+                $rows = 0;
+                oci_define_by_name($query_check_ced, "NUM_ROWS", $rows);
+                ociexecute($query_check_ced);
+                ocifetch($query_check_ced);
 
-            //**********************************************************************************
-            if ($rows > 0) {
-                echo "<section id='error' style='position:absolute; top:10px; left:350px; background-color:#ff3e3e;'>
-                <a style='font-size:20px; color:#000;'>La cédula jurídica " . $cedJuridica . " ya se encuentra registrada.</a>
-                </section>";
-                echo $def;
+                //**********************************************************************************
+                if ($rows > 0) {
+                    echo "<section id='error' style='position:absolute; top:170px; left:545px;'>
+                    <a style='font-size:20px; color:#F00; font-size:16px;'>**La cédula jurídica " . $cedJuridica . " ya se encuentra registrada.</a>
+                    </section>";
+                    
+                } else {
+                    
+                    
+                    if($categoria == 'otra') {
+                        $categoria2 = 'hoslis';//$_POST['categoria2'];
+                        $descripcion = 'DI esta';//$_POST['descripcion'];    
+                        $categoria = $categoria2;
+                        
+                        $check_existe_cat = "SELECT COUNT(*) AS NUM_ROWS FROM categoria WHERE nombre=:categoria and tipo = 'E'";
+                        $query_check_existe_cat = ociparse($conn, $check_existe_cat);
+                        ocibindbyname($query_check_existe_cat, ":categoria", $categoria);
+                        $existe_cat = 0;
+                        oci_define_by_name($query_check_existe_cat, "NUM_ROWS", $existe_cat);
+                        ociexecute($query_check_existe_cat);
+                        ocifetch($query_check_existe_cat);
+                        
+                        if($existe_cat == 0) {
+                            
+                            //registra la nueva categoria
+                            $createcat = "begin pack_categoria.set_categoria(:nombre, :descripcion, 'E'); end;";
+                            $query_createcat = ociparse($conn, $createcat);
+                            ocibindbyname($query_createcat, ":nombre", $categoria);
+                            ocibindbyname($query_createcat, ":descripcion", $descripcion);
+                            ociexecute($query_createcat);
+                            $existe_cat = 1;
+                            
+                        } else {
+                            $existe_cat = -1;
+                            echo "<section id='error' style='position:absolute; top:170px; left:545px;'>
+                            <a style='font-size:20px; color:#F00; font-size:16px;'>**La categoria " . $categoria . " ya se encuentra registrada.</a>
+                            </section>";
+                               
+                        }
+                    }
+                    
+                    if($categoria != 'otra' and $existe_cat > 0) {
+                        $puede = true;
+                    } elseif($categoria == 'otra' and $existe_cat == 0) {
+                        $puede = true;
+                    } else {
+                        $puede = false;
+                    }
+
+                    if($puede) {
+                        
+                    
+                        //registra la nueva entidad
+                        $setentidad = "begin pack_entidad.set_entidad(:nombre, :cedJuridica); end;";
+                        $query_setentidad = ociparse($conn, $setentidad);
+                        ocibindbyname($query_setentidad, ":nombre", $nombre);
+                        ocibindbyname($query_setentidad, ":cedJuridica", $cedJuridica);
+                        ociexecute($query_setentidad);
+
+                         //registra la nueva categoria
+                        $setcat = "begin pack_categoria_entidad.set_categoria_entidad(pack_categoria.get_id(:categoria), :cedJuridica); end;";
+                        $query_setcat = ociparse($conn, $setcat);
+                        ocibindbyname($query_setcat, ":categoria", $categoria);
+                        ocibindbyname($query_setcat, ":cedJuridica", $cedJuridica);
+                        ociexecute($query_setcat);
+                        echo "entro al final";
+                        
+                        //registra la direccion de la entidad nueva
+                        $setdireccion = "begin pack_direccion_entidad.set_direccion_entidad(:direccionExacta, :barrio); end;";
+                        $query_setdireccion = ociparse($conn, $setdireccion);
+                        ocibindbyname($query_setdireccion, ":barrio", $barrio);
+                        ocibindbyname($query_setdireccion, ":direccionExacta", $direccionExacta);
+                        ociexecute($query_setdireccion);
+                        
+                    }
+                }
             } else {
-                
-                //registra la nueva entidad
-                $setentidad = "begin pack_entidad.set_entidad(:nombre, :cedJuridica); end;";
-                $query_setentidad = ociparse($conn, $setentidad);
-                ocibindbyname($query_setentidad, ":nombre", $nombre);
-                ocibindbyname($query_setentidad, ":cedJuridica", $cedJuridica);
-                ociexecute($query_setentidad);
-                
-                //registra la direccion de la entidad nueva
-                $setdireccion = "begin pack_direccion_entidad.set_direccion_entidad(:direccionExacta, :barrio); end;";
-                $query_setdireccion = ociparse($conn, $setdireccion);
-                ocibindbyname($query_setdireccion, ":barrio", $barrio);
-                ocibindbyname($query_setdireccion, ":direccionExacta", $direccionExacta);
-                ociexecute($query_setdireccion);
+                //mensaje de error
+                echo "<script> alert('La cédula debe de ser un número.') </script>";
             }
-            
         } else {
             //mensaje de error
-            echo "<section id='error' style='position:absolute; top:10px; left:350px; background-color:#ff3e3e;'>
-            <a style='font-size:20px; color:#000;'>Debe de llenar los espacios.</a>
+            echo "<section id='error' style='position:absolute; top:170px; left:545px;'>
+            <a style='font-size:20px; color:#F00; font-size:16px;'>**El largo máximo es de 25, la cédula de 9 y la dirección exacta de 50.</a>
             </section>";
-            echo $def;  
+              
         }
     
     } else {
         //mensaje de error
-        echo "<section id='error' style='position:absolute; top:10px; left:350px; background-color:#ff3e3e;'>
-        <a style='font-size:20px; color:#000;'>Debe de llenar los espacios.</a>
+        echo "<section id='error' style='position:absolute; top:170px; left:545px;'>
+        <a style='font-size:20px; color:#F00; font-size:16px;'>**Debe de llenar los espacios.</a>
         </section>";
-        echo $def;  
+          
     }
 ?>
