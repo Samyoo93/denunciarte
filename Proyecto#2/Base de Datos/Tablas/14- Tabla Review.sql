8704a074a8948b58662a745246ea212c7cb37871
BarCB2011 <bar_cas_bar@yahoo.com>
2014-05-19 11:05:55 -0600

    Update 14- Tabla Review.sql

--- Proyecto#2/Base de Datos/Tablas/14- Tabla Review.sql
+++ Proyecto#2/Base de Datos/Tablas/14- Tabla Review.sql
@@ -9,8 +9,10 @@ CREATE TABLE Review
           CONSTRAINT descripcion_review_nn NOT NULL,
      calificacion NUMBER(2)
           CONSTRAINT calificacion_nn NOT NULL,
-     cedulaUsuario_id_fk NUMBER(9)
+     cedulaUsuario_id NUMBER(9)
           CONSTRAINT cedulaUsuario_id_fk_review_nn NOT NULL,
+          CONSTRAINT cedulaUsuario_id_fk_review  FOREIGN KEY (cedulaUsuario_id)
+               REFERENCES usuario(cedulaUsuario_id),
      fecha_creacion DATE,
      usuario_creacion VARCHAR2(25),
      fec_ultima_modificacion DATE,
