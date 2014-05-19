d58593ae7455a4f9d8536d65369403486f617906
BarCB2011 <bar_cas_bar@yahoo.com>
2014-05-19 10:57:14 -0600

    Update 2- Tabla PersonaFisica.sql

--- Proyecto#2/Base de Datos/Tablas/2- Tabla PersonaFisica.sql
+++ Proyecto#2/Base de Datos/Tablas/2- Tabla PersonaFisica.sql
@@ -3,9 +3,9 @@ CREATE TABLE PersonaFisica
      cedulaFisica_id NUMBER(9)
           CONSTRAINT cedulaFisica_id_nn NOT NULL
           CONSTRAINT cedulaFisica_id_pk PRIMARY KEY, 
-     persona_id_fk NUMBER(6)
+     persona_id NUMBER(6)
           CONSTRAINT persona_id_fk_PersonaFisica_nn NOT NULL,
-          CONSTRAINT persona_id_fk_PersonaFisica FOREIGN KEY (persona_id_fk)
+          CONSTRAINT persona_id_fk_PersonaFisica FOREIGN KEY (persona_id)
                REFERENCES persona(persona_id),
      fecha_creacion DATE,
      usuario_creacion VARCHAR2(25),
