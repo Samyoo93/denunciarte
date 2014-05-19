915640fcfa269444e618f6788ab78ce9813ced97
BarCB2011 <bar_cas_bar@yahoo.com>
2014-05-19 10:55:05 -0600

    Update 4- Tabla Categoria_PersonaFisica.sql

--- Proyecto#2/Base de Datos/Tablas/4- Tabla Categoria_PersonaFisica.sql
+++ Proyecto#2/Base de Datos/Tablas/4- Tabla Categoria_PersonaFisica.sql
@@ -3,13 +3,13 @@ CREATE TABLE Categoria_PersonaFisica
      categoria_persona_id NUMBER(6)
           CONSTRAINT categoria_pesona_id_nn NOT NULL
           CONSTRAINT  pk_categoria_pesona_id PRIMARY KEY ,     
-     cedulaFisica_id_fk NUMBER(9)
+     cedulaFisica_id NUMBER(9)
           CONSTRAINT fisica_id_fk_CatPerFis_nn NOT NULL,
-          CONSTRAINT fisica_id_fk_CatPerFis FOREIGN KEY(cedulaFisica_id_fk)
+          CONSTRAINT fisica_id_fk_CatPerFis FOREIGN KEY(cedulaFisica_id)
                REFERENCES personaFisica(cedulaFisica_id),
-     categoria_id_fk NUMBER(6)
+     categoria_id NUMBER(6)
           CONSTRAINT categoria_id_fk_CatPerFis_nn NOT NULL,
-          CONSTRAINT categoria_id_fk_CatPerFis FOREIGN KEY(categoria_id_fk)
+          CONSTRAINT categoria_id_fk_CatPerFis FOREIGN KEY(categoria_id)
                REFERENCES categoria(categoria_id),
      fecha_creacion DATE,
      usuario_creacion VARCHAR2(25),
