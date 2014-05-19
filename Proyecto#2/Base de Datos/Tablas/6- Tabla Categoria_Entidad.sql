409606429aea30a463bf209289855a569792dd10
BarCB2011 <bar_cas_bar@yahoo.com>
2014-05-19 10:54:20 -0600

    Update 6- Tabla Categoria_Entidad.sql

--- Proyecto#2/Base de Datos/Tablas/6- Tabla Categoria_Entidad.sql
+++ Proyecto#2/Base de Datos/Tablas/6- Tabla Categoria_Entidad.sql
@@ -3,13 +3,13 @@ CREATE TABLE Categoria_Entidad
      categoria_entidad_id  NUMBER(6)
           CONSTRAINT catEnt_Entidades_id_pk PRIMARY KEY
           CONSTRAINT catEnt_Entidades_id_nn NOT NULL,
-     categoria_id_fk NUMBER(6)
+     categoria_id NUMBER(6)
           CONSTRAINT categoria_id_fk_CatEnt_nn NOT NULL,
-          CONSTRAINT categoria_id_fk_CatEnt FOREIGN KEY(categoria_id_fk)
+          CONSTRAINT categoria_id_fk_CatEnt FOREIGN KEY(categoria_id)
                REFERENCES categoria(categoria_id),
-     entidad_id_fk NUMBER(6)
+     entidad_id NUMBER(6)
           CONSTRAINT entidad_id_fk_CatEnt_nn NOT NULL,
-          CONSTRAINT entidad_id_fk_CatEnt FOREIGN KEY(categoria_id_fk)
+          CONSTRAINT entidad_id_fk_CatEnt FOREIGN KEY(categoria_id)
                REFERENCES categoria(categoria_id),
      fecha_creacion DATE,
      usuario_creacion VARCHAR2(25),
