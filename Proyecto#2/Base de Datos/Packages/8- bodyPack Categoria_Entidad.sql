ce72da5233fe6082a37467576bfa951bcf9c8ad0
BarCB2011 <bar_cas_bar@yahoo.com>
2014-05-19 10:42:06 -0600

    Update 8- bodyPack Categoria_Entidad.sql

--- Proyecto#2/Base de Datos/Packages/8- bodyPack Categoria_Entidad.sql
+++ Proyecto#2/Base de Datos/Packages/8- bodyPack Categoria_Entidad.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE PACKAGE BODY pack_categoria_entidad AS
           IS
           BEGIN
                INSERT INTO categoria_entidad
-                    (categoria_entidad_id, entidad_id_fk, categoria_id_fk)
+                    (categoria_entidad_id, entidad_id, categoria_id)
                VALUES
                     (s_categoria_entidad.nextval, entidadId, categoriaId);
 	       COMMIT;
