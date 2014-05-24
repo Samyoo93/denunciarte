2df540e9ad00b4d2ef11f3f1221a17bde6e56ef0
fransa2910 <franco.solis2910@hotmail.com>
2014-05-24 12:18:03 -0600

    update

--- Proyecto#2/denunciARTE_PHP/Interfaz/entidad.php
+++ Proyecto#2/denunciARTE_PHP/Interfaz/entidad.php
@@ -5,39 +5,67 @@
 <title>Denunciarte</title>
 <link rel="stylesheet" href="Estilo/Estilo.css" />
 <script type="text/javascript">
-function registrar(){
-    // Create our XMLHttpRequest object
-    var hr = new XMLHttpRequest();
-    // Create some variables we need to send to our PHP file
-    var url = "registrarUsuario.php";
-    var nombre = document.getElementById("nombre").value;
-    var primerApellido = document.getElementById("primerApellido").value;
-    var segundoApellido = document.getElementById("segundoApellido").value;
-    var fecNac = document.getElementById("fecNac").value;
-    var genero = document.getElementById("genero").value;
-    var contrasena = document.getElementById("contrasena").value;
-	var contrasena2 = document.getElementById("contrasena2").value;
-    var usuario = document.getElementById("usuario").value;
-	var nick= document.getElementById("nick").value;
-	var cedula1= document.getElementById("cedula1").value;
-	var cedula2=document.getElementById("cedula2").value;
-	var cedula3= document.getElementById("cedula3").value;
+function crear(){
+        // Create our XMLHttpRequest object
+        var hr = new XMLHttpRequest();
+        // Create some variables we need to send to our PHP file
+        var url = "registroEntidad.php";
+        var nombre = document.getElementById('nombre').value;
+        var cedJuridica = document.getElementById('cedJuridica').value;
+        var pais = document.getElementById('pais').value;
+        var provincia = document.getElementById('provincia').value;
+        var canton = document.getElementById('canton').value;
+        var distrito = document.getElementById('distrito').value;
+        var barrio = document.getElementById('barrio').value;
+        var tipoCategoria = document.getElementById('tipoCategoria').value;
+        var categoria2 = document.getElementById('categoria2').value;
+        var direccionExacta = document.getElementById('direccionExacta').value;
+        var descripcion = document.getElementById('descripcion').value;
 
+        var vars = 'nombre='+nombre+'&cedJuridica='+cedJuridica+'&pais='+pais+'&provincia='+provincia+'&canton='+canton+'&distrito='+distrito+
+            '&barrio='+barrio+'&tipoCategoria='+tipoCategoria+'&categoria2='+categoria2+'&direccionExacta='+direccionExacta+
+            '&descripcion='+descripcion;
+        hr.open("POST", url, true);
+        // Set content type header information for sending url encoded variables in the request
+        hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        // Access the onreadystatechange event for the XMLHttpRequest object
+        hr.onreadystatechange = function() {
+            if(hr.readyState == 4 && hr.status == 200) {
+                var return_data = hr.responseText;
+                document.getElementById("crearEntidad").innerHTML = return_data;
+            }
+        }
+        // Send the data to PHP now... and wait for response to update the status div
+        hr.send(vars); // Actually execute the request
+        document.getElementById("crearEntidad").innerHTML = "procesando...";
+	}
+    function refresh(changed){
+        // Create our XMLHttpRequest object
+        var hr = new XMLHttpRequest();
+        // Create some variables we need to send to our PHP file
+        var url = "locationRefreshEntidad.php";
+        var which = changed;
 
-    var vars = 'nombre='+nombre+'&primerApellido='+primerApellido+"&segundoApellido="+segundoApellido+'&fecNac='+fecNac+'&nick='+nick+'&cedula1='+cedula1+'&cedula2='+cedula2+'&cedula3='+cedula3+'&genero='+genero+'&usuario='+usuario+'&contrasena='+contrasena+'&contrasena2='+contrasena2;
-    hr.open("POST", url, true);
-    // Set content type header information for sending url encoded variables in the request
-    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // Access the onreadystatechange event for the XMLHttpRequest object
-    hr.onreadystatechange = function() {
-	    if(hr.readyState == 4 && hr.status == 200) {
-		    var return_data = hr.responseText;
-			document.getElementById("registro").innerHTML = return_data;
-	    }
-    }
-    // Send the data to PHP now... and wait for response to update the status div
-    hr.send(vars); // Actually execute the request
-    document.getElementById("registro").innerHTML = "procesando...";
+        var pais = document.getElementById("pais").value;
+        var provincia = document.getElementById("provincia").value;
+        var canton = document.getElementById("canton").value;
+        var distrito = document.getElementById("distrito").value;
+        var barrio = document.getElementById("barrio").value;
+
+        var vars = 'pais='+pais+'&provincia='+provincia+'&canton='+canton+'&distrito='+distrito+'&barrio='+barrio+'&which='+which;
+        hr.open("POST", url, true);
+        // Set content type header information for sending url encoded variables in the request
+        hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        // Access the onreadystatechange event for the XMLHttpRequest object
+        hr.onreadystatechange = function() {
+            if(hr.readyState == 4 && hr.status == 200) {
+                var return_data = hr.responseText;
+                document.getElementById("direccion").innerHTML = return_data;
+            }
+        }
+        // Send the data to PHP now... and wait for response to update the status div
+        hr.send(vars); // Actually execute the request
+        document.getElementById("direccion").innerHTML = "procesando...";
 	}
 	</script>
 </head>
@@ -66,29 +94,51 @@ function registrar(){
 <input type="text" id="nombre" style="position:absolute; top:90px; left:130px; width:300px;" />
 <a style="position:absolute; top:130px; left:60px;">Cédula Jurídica</a>
 <input type="text" id="cedJuridica" style="position:absolute; top:150px; left:130px; width:300px;"/>
+<div id='direccion'>
+
 <h2 style="position:absolute; top:170px; left:60px;">Dirección</h2>
 <a style="position:absolute; top:210px; left:60px;">__________</a>
 <a style="position:absolute; top:250px; left:60px;">País</a>
 <select name='pais' required style="position:absolute; top:250px; text-align:center;
 left:130px; width:300px;">
 </select>
+    <?php
+		include('../conection.php');
+		$conn = oci_connect($user, $pass, $db);
+		$sql = "SELECT nombre FROM pais";
+		$stmt = oci_parse($conn, $sql);
+		ociexecute($stmt);
+		echo "<select name='pais' required onchange='refresh(1)' style="position:absolute; top:250px; text-align:center;
+left:130px; width:300px;">";
+        echo "<option value=''>Seleccione uno</option>";
+		while ( $row = oci_fetch_assoc($stmt) ) {
+
+			if($row['NOMBRE']==$pais) {
+				echo "<option selected value='$row[NOMBRE]'>$row[NOMBRE]</option>"."<BR>";
+			} else {
+				echo "<option value='$row[NOMBRE]'>$row[NOMBRE]</option>"."<BR>";
+			}
+		}
+
+	echo "</select>"; ?>
 
 <a style="position:absolute; top:290px; left:60px;">Provincia</a>
-<select name='provincia' required style="position:absolute; top:290px; text-align:center;
+<select name='provincia' id='provincia' onchange='refresh(2)' required style="position:absolute; top:290px; text-align:center;
 left:130px; width:300px;">
 </select>
 <a style="position:absolute; top:330px; left:60px;">Cantón</a>
-<select name='canton' required style="position:absolute; top:330px; text-align:center;
+<select name='canton' required id='canton' onchange='refresh(3)'style="position:absolute; top:330px; text-align:center;
 left:130px; width:300px;">
 </select>
 <a style="position:absolute; top:370px; left:60px;">Distrito</a>
-<select name='distrito' required style="position:absolute; top:370px; text-align:center;
+<select name='distrito' required id='distrito' onchange='refresh(4)'style="position:absolute; top:370px; text-align:center;
 left:130px; width:300px;">
 </select>
 <a style="position:absolute; top:400px; left:60px;">Barrio</a>
-<select name='barrio' required style="position:absolute; top:400px; text-align:center;
+<select name='barrio' required id='barrio' onchange='refresh(5)'style="position:absolute; top:400px; text-align:center;
 left:130px; width:300px;">
 </select>
+</div>
 <a style="position:absolute; top:430px; left:60px;"> Dirección exacta </a>
 <textarea style="position:absolute; top:450px; left:130px; width:290px; height:50px;" ></textarea>
 <h2 style="position:absolute; top:490px; left:60px;">Categoría</h2>
@@ -115,7 +165,8 @@ left:130px; width:300px;">
 </section>
 </section>
 
-
+<div id='crearEntidad'>
+</div>
 <!-- Pie de página -->
 <section id="CuadroGris" style=" top:965px; position:absolute; left:20px; width:960px; height:90px">
 <a style="position:absolute; left:20px; top:10px;"> Desarrolladores </a>
