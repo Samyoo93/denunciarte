1c1298de2817773c245374afd53cac84443908d2
Kathy20 <kathy.20@hotmail.es>
2014-05-24 23:11:31 -0600

    Update UpdateEntidad.php

---
+++ Proyecto#2/denunciARTE_PHP/Interfaz/Perfil/UpdateEntidad.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>DenunciARTE</title>
+    <link rel="stylesheet" href="../Estilo/Estilo.css" />
+</head>
+
+<body style="width:700px;" onload="Abrir_ventana('http://URL/ejemplo-popup.html')">
+
+
+<section style="position:absolute; left:200px; top:100px; width:630px; height:400px;">
+<div id="mostrar" style="overflow-y:scroll;">
+<h1 style="position:absolute; left:150px; left:120px;"> Modificar Entidad </h1>
+<a style="position:absolute; top:150px; left:70px;">Nombre</a>
+<input type="text" id="nombre" style="position:absolute; left:200px; top:150px; width:300px;">
+<a style="position:absolute; top:180px; left:70px;">Cédula Jurídica</a>
+    <input type="text" id="cedulaJuridica" style="position:absolute; left:200px; top:180px; width:300px;">
+<h2 style="position:absolute; top:200px; left:70px;">Dirección</h2>
+<a style="position:absolute; top:240px; left:70px;">_________</a>
+<a style="position:absolute; top:270px; left:70px;">País</a>
+<select id="pais" style="position:absolute; left:200px; top:270px; width:300px;">
+</select>
+<a style="position:absolute; top:300px; left:70px;">Provincia</a>
+<select id="provincia" style="position:absolute; left:200px; top:300px; width:300px;">
+</select>
+<a style="position:absolute; top:330px; left:70px;">Cantón</a>
+<select id="canton" style="position:absolute; left:200px; top:330px; width:300px;">
+</select>
+<a style="position:absolute; top:360px; left:70px;">Distrito</a>
+<select id="distrito" style="position:absolute; left:200px; top:360px; width:300px;">
+</select>
+<a style="position:absolute; top:390px; left:70px;">Dirección Exacta</a>
+<textarea style="position:absolute; top:410px; left:200px; width:300px; height:100px;"></textarea>
+<button type="submit" style="position:absolute; top:550px; left:220px; width:200px;">Actualizar</button>
+
+</div>
+</section>
+
+<!-- Pie de página -->
+<section id="CuadroGris" style=" top:810px; position:absolute; left:20px; width:960px; height:90px">
+<a style="position:absolute; left:20px; top:10px;"> Desarrolladores </a>
+<a style="position:absolute; left:40px; top:40px;"> -Kathy Brenes G. </a>
+<a style="position:absolute; left:190px; top:40px;"> -Barnum Castillo B. </a>
+<a style="position:absolute; left:340px; top:40px;"> -Franco Solís A. </a>
+<a style="position:absolute; left:480px; top:40px;"> -Samuel Yoo. </a>
+
+<a style=" position:absolute; top:70px; left:700px;">DenunciARTE © 2014 · Español </a>
+
+</section>
+
+<!-- Encabezado-->
+<section id="CuadroGris" style="position:absolute; left:20px; height:90px; width:960px;">
+<img src="../Imagenes/Denunciarteicono.jpg" style="position:absolute; left:0px;" />
+<input type=search results=5 placeholder='Buscar entidad, persona.'  name=busqueda style="position:absolute; left:95px; top:30px; width:300px;">
+<a href=""  style="position:absolute; top:70px; left:125px;">+Busqueda avanzada</a>
+<button type="submit" style="position:absolute; top:20px; left:400px;">Buscar</button>
+<section style="position:absolute; left:560px;">
+<nav align="center" >
+<ul id="menu">
+	 <li><a title="Perfil" href=”#”>Usuario</a></li>
+    <li><a title="Inicio" href="InicioUsuarios.php">Inicio</a></li>
+
+ 	<li><a title="Privacidad"> <img src="../Imagenes/candado.png" /></a>
+ 	<ul>
+     <li style="font-size:16px; width:150px;"><a href="">Configuración</a></li>
+    <li style="font-size:16px; width:150px;"><a href="">Privacidad</a></li>
+  </ul>
+ </li>
+    <li style="width:60px; height:60px;"><img src="../Imagenes/flechafinal.png" style="position:absolute; top:40px;" />
+    	<ul>
+        	<li style="font-size:16px; width:150px;"><a href="">Crear una entidad</a></li>
+     		<li style="font-size:16px; width:150px;"><a href="">Cerrar sesión</a></li>
+    		<li style="font-size:16px; width:150px;"><a href="">Ayuda</a></li>
+  		</ul>
+  </li>
+    <li>
+</ul>
+</nav>
+</section>
+</section>
+
+</body>
+</html>
