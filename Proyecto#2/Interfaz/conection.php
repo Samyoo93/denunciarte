fd320f277957206023da94d35c257e98d9936a82
Kathy20 <kathy.20@hotmail.es>
2014-05-27 13:44:26 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Proyecto#2/Interfaz/Perfil/Usuario.php

--- Proyecto#2/Interfaz/conection.php
+++ Proyecto#2/Interfaz/conection.php
@@ -2,6 +2,6 @@
 //aqui especifica el usuario, password, y nombre de base de datos de oracle
 	$user='DenunciARTE';	//OJO AQUI ESCRIBEN EL NOMBRE DE SU PROPIO USUARIO
 	$pass='DenunciARTE';	//SU PROPIO PASSWORD Y
-	$db='DenunciARTE';	//EL NOMBRE DE SU BASE DE DATOS
+	$db='KathyORACE';	//EL NOMBRE DE SU BASE DE DATOS
 
 ?>
