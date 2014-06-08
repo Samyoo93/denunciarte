272a5033aa295828211879a551e6a8bfc472e8a3
fransa2910 <franco.solis2910@hotmail.com>
2014-06-08 15:08:06 -0600

    acomodo mostrar datos

--- Proyecto#2/Interfaz/Perfil/mostrarDatos.php
+++ Proyecto#2/Interfaz/Perfil/mostrarDatos.php
@@ -251,13 +251,13 @@
                 <form action="../hovercard/pasarValorALaBase.php" method="post" enctype="multipart/form-data">
                     <a href="#close" title="Close" class="close">X</a>
                     <h2>Calificar a esta persona</h2>
-                    <p style="position:absolute; top:70px;">Si desea calificar a '. $nombre .', rellene los siguientes campos:</p>
+                    <p style="position:absolute; top:60px;">Si desea calificar a '. $nombre .', rellene los siguientes campos:</p>
                     <p style="position:absolute; top:130px;">Título</p>
                     <input type="text" name ="titulo" style="position:absolute; top:150px; left: 150px; width:200px;">
                     <p style="position:absolute; top:160px;">Descripción</p>
                     <textarea type="text" name="descripcion" style="position:absolute; top:180px; left: 150px;width:300px; height:100px;"></textarea>
                     <p style="position:absolute; top:280px;">Calificación</p>
-                    <a>File name:</a> <input type="file" required id="imgfile" "position:absolute; top:200px; left:130px;" name="imgfile"><br>
+                    <a style="position:absolute; top:115px;">Archivo:</a> <input type="file" required id="imgfile" style="position:absolute; top:115px; left:145px;" name="imgfile"><br>
 
                     <div class="rateit" id="estrellas" data-rateit-max="10" data-rateit-step=1 data-rateit-value=1 data-rateit-resetable="false"  style="position:absolute; top:300px; left:150px;">
                         <input type="number" class="numCalf" name="estrellotas">
