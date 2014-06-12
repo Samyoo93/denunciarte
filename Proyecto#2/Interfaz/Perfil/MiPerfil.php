341c0c57e20b789cb4a9260f1e79d9b950c815db
Kathy20 <kathy.20@hotmail.es>
2014-06-11 22:12:24 -0600

    comments

--- Proyecto#2/Interfaz/Perfil/MiPerfil.php
+++ Proyecto#2/Interfaz/Perfil/MiPerfil.php
@@ -1,5 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+    INSTITUTO TECNOLÓGICO DE COSTA RICA
+    BASES DE DATOS I
+    I SEMESTRE 2014
+    II PROYECTO
+
+    DENUNCIARTE
+
+    ESTUDIANTES
+    KATHY BRENES GUERRERO.
+    BARNUM CASTILLO BARQUERO.
+    FRANCO SOLÍS ALVARADO.
+    SAM YOO.
+
+    Nombre del archivo: MiPerfil.php
+    Descripción: Plantilla que permite mostrar los datos proporcionados
+    en el registro del usuario que ha iniciado sesión.
+-->
 <head>
     <?php
     session_start();
